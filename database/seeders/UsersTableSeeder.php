694bbed5fcc272ab79499cedd636f9d72147bc7a
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-03-02 16:47:31 +0800

    add user seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->delete();
+
+        \DB::statement('ALTER SEQUENCE users_id_seq RESTART WITH 2');
+
+        \DB::table('users')->insert(array(
+            0 =>
+            array(
+                'id' => 1,
+                'name' => 'Eksa Pramindanata',
+                'username' => 'pramindanata',
+                'password' => '$2y$10$8vadgb/pvw7h.Djs.paxpelmYl7Gu6V3/jksD/d8iJc4X4OeYD4Du',
+                'role' => 'ADMIN',
+                'remember_token' => 'g1rON6gdfouAb0ktoTCIplJNGS0ztuHxBBvzinkj84nFprEVqayzZ31KlAYr',
+                'created_at' => '2020-09-26 17:30:39+08',
+                'updated_at' => '2020-09-26 17:30:39+08',
+            ),
+        ));
+    }
+}
