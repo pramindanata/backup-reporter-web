c4e71f29ef9e4c97a78f56d4de4fb9bd8ae6e47b
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-03-02 17:04:13 +0800

    update user list page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,9 +6,7 @@
                     <li class="nav-item {{ $getActiveClassBySegment(null) }}">
                         <a class="nav-link" href="{{ route('home') }}">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home"
-                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
                                     <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
@@ -25,7 +23,7 @@
                     <li class="nav-item {{ $getActiveClassBySegment('users') }}">
                         <a class="nav-link" href="{{ route('users.index') }}">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <circle cx="9" cy="7" r="4"></circle>
                                     <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -1,12 +1,27 @@
 <x-layouts.main title="User List">
     <div class="container-xl">
-        <!-- Page title -->
         <div class="page-header d-print-none">
             <div class="row align-items-center">
                 <div class="col">
                     <h2 class="page-title">
                         User List
                     </h2>
+                    <div class="text-muted mt-1">1 of 241 data</div>
+                </div>
+
+                <div class="col-auto ms-auto d-print-none">
+                    <div class="d-flex">
+                        <a href="{{ route('users.create') }}" class="btn btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <line x1="12" y1="5" x2="12" y2="19"></line>
+                                <line x1="5" y1="12" x2="19" y2="12"></line>
+                            </svg>
+                            Add new
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
